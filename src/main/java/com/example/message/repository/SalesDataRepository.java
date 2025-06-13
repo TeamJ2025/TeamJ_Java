7b882c22d851b2982c8f20abf651ec8a6b632ac2
asterkaka <yuejiapan.00@gmail.com>
2025-06-13 11:48:58 +0900

    売上データ表示の見た目を修正した（はんえつか）。

--- src/main/java/com/example/message/repository/SalesDataRepository.java
+++ src/main/java/com/example/message/repository/SalesDataRepository.java
@@ -3,7 +3,8 @@ package com.example.message.repository;
 import com.example.message.entity.SalesData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDate;
-
+import java.util.List;
 
 public interface SalesDataRepository extends JpaRepository<SalesData, LocalDate> {
+    List<SalesData> findAllByOrderBySaleDayDesc();
 }

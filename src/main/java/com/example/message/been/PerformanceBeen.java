f730245dae515752318b3e54699b204ec96af522
Miyu245 <sugiuram0121@gmail.com>
2025-06-16 11:52:28 +0900

    beenを削除、SalesData.javaをデータの参照に使用していく。

--- src/main/java/com/example/message/been/PerformanceBeen.java
+++ src/main/java/com/example/message/been/PerformanceBeen.java
@@ -1,58 +1,58 @@
-package com.example.message.been;
+// package com.example.message.been;
 
-public class PerformanceBeen {
-    private int whiteBeerBottles;
-    private int lagerBottles;
-    private int paleAleBottles;
-    private int fruitBeerBottles;
-    private int blackBeerBottles;
-    private int ipaBottles;
+// public class PerformanceBeen {
+//     private int whiteBeerBottles;
+//     private int lagerBottles;
+//     private int paleAleBottles;
+//     private int fruitBeerBottles;
+//     private int blackBeerBottles;
+//     private int ipaBottles;
 
-    public int getWhiteBeerBottles() {
-        return whiteBeerBottles;
-    }
+//     public int getWhiteBeerBottles() {
+//         return whiteBeerBottles;
+//     }
 
-    public void setWhiteBeerBottles(int whiteBeerBottles) {
-        this.whiteBeerBottles = whiteBeerBottles;
-    }
+//     public void setWhiteBeerBottles(int whiteBeerBottles) {
+//         this.whiteBeerBottles = whiteBeerBottles;
+//     }
 
-    public int getLagerBottles() {
-        return lagerBottles;
-    }
+//     public int getLagerBottles() {
+//         return lagerBottles;
+//     }
 
-    public void setLagerBottles(int lagerBottles) {
-        this.lagerBottles = lagerBottles;
-    }
+//     public void setLagerBottles(int lagerBottles) {
+//         this.lagerBottles = lagerBottles;
+//     }
 
-    public int getPaleAleBottles() {
-        return paleAleBottles;
-    }
+//     public int getPaleAleBottles() {
+//         return paleAleBottles;
+//     }
 
-    public void setPaleAleBottles(int paleAleBottles) {
-        this.paleAleBottles = paleAleBottles;
-    }
+//     public void setPaleAleBottles(int paleAleBottles) {
+//         this.paleAleBottles = paleAleBottles;
+//     }
 
-    public int getFruitBeerBottles() {
-        return fruitBeerBottles;
-    }
+//     public int getFruitBeerBottles() {
+//         return fruitBeerBottles;
+//     }
 
-    public void setFruitBeerBottles(int fruitBeerBottles) {
-        this.fruitBeerBottles = fruitBeerBottles;
-    }
+//     public void setFruitBeerBottles(int fruitBeerBottles) {
+//         this.fruitBeerBottles = fruitBeerBottles;
+//     }
 
-    public int getBlackBeerBottles() {
-        return blackBeerBottles;
-    }
+//     public int getBlackBeerBottles() {
+//         return blackBeerBottles;
+//     }
 
-    public void setBlackBeerBottles(int blackBeerBottles) {
-        this.blackBeerBottles = blackBeerBottles;
-    }
+//     public void setBlackBeerBottles(int blackBeerBottles) {
+//         this.blackBeerBottles = blackBeerBottles;
+//     }
 
-    public int getIpaBottles() {
-        return ipaBottles;
-    }
+//     public int getIpaBottles() {
+//         return ipaBottles;
+//     }
 
-    public void setIpaBottles(int ipaBottles) {
-        this.ipaBottles = ipaBottles;
-    }
-}
+//     public void setIpaBottles(int ipaBottles) {
+//         this.ipaBottles = ipaBottles;
+//     }
+// }
--- src/main/java/com/example/message/controller/PerformanceController.java
+++ src/main/java/com/example/message/controller/PerformanceController.java
@@ -1,50 +1,54 @@
 package com.example.message.controller;
 
+import com.example.message.entity.SalesData;
+import com.example.message.service.PerformanceService;
+import com.example.message.util.ConfirmViewHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.ui.Model;
+
 import java.util.Map;
-import com.example.message.been.PerformanceBeen;
-import com.example.message.service.PerformanceService;
-import com.example.message.util.ConfirmViewHelper;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PerformanceController {
-    
+
     @Autowired
     private PerformanceService performanceService;
-
-    @RequestMapping("/Performance/PerformanceView")
-    public String start(){
-        return "PerformanceView.html";
-    }
-    
-    @RequestMapping("/Performance/Input")
-    public String input(){
-        return "Input.html";
-    }
 
     @Autowired
     private ConfirmViewHelper confirmViewHelper;
 
+    @RequestMapping("/Performance/PerformanceView")
+    public String start() {
+        return "PerformanceView.html";
+    }
+
+    @RequestMapping("/Performance/Input")
+    public String input(Model model) {
+        model.addAttribute("salesData", new SalesData());
+        return "Input.html";
+    }
+
     @PostMapping("/Performance/Confirm")
-    public ModelAndView confirm(@ModelAttribute PerformanceBeen been){
+    public ModelAndView confirm(@ModelAttribute SalesData salesData) {
         ModelAndView mv = new ModelAndView("Confirm.html");
 
-        Map<String, Integer> amounts = performanceService.calculateAmounts(been);
-        int totalSalesYen = performanceService.calculateTotalAmount(amounts);
-        int totalCups = performanceService.calculateTotalCount(been);
+        Map<String, Integer> amounts = performanceService.calculateAmounts(salesData);
+        int totalSalesYen = performanceService.calculateTotalSalesYen(amounts);
+        int totalCups = performanceService.calculateTotalCount(salesData);
 
-        confirmViewHelper.setupConfirmModel(mv, been, amounts, totalCups, totalSalesYen);
+        confirmViewHelper.setupConfirmModel(mv, salesData, amounts, totalCups, totalSalesYen);
 
         return mv;
     }
+
     @PostMapping("/Performance/Complete")
-    public String complete(@ModelAttribute("been") PerformanceBeen been) {
-        performanceService.savePerformance(been); // ここでDB保存
+    public String complete(@ModelAttribute("salesData") SalesData salesData) {
+        performanceService.savePerformance(salesData);
         return "Complete.html";
     }
 }
